835870cd10556f3c37a7174d0f5df8cdba39107c
tanvir1990 <pavel.mhp@gmail.com>
2021-01-12 17:22:38 -0500

    Completed the UDP code

--- Labs/Lab1/UDP_Pinger_client.py
+++ Labs/Lab1/UDP_Pinger_client.py
@@ -1,18 +1,24 @@
-# Lab1
+# Lab1, SYSC 4502
 # Tanvir Hossain
 # Student ID: 101058988
+
 from socket import*
 from datetime import datetime
 import time
+import sys
 
-serverName = '127.0.0.1'
-serverPort = 12000
+# Taking the arguments from Command line input
+server_name = str(sys.argv[1])
+server_port = int(sys.argv[2])
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.connect((serverName, serverPort))
-clientSocket.settimeout(1)
+# Create socket Server
+socket_client = socket(AF_INET, SOCK_DGRAM)
+socket_client.connect((server_name, server_port))                          # Create a connection
+socket_client.settimeout(1)                                              # Connection timeout set to 1s
 
-# Creating 10 Ping Messages
+# Creating 10 Ping Messages into an array of String messages
+# It could have been done inside the for loop as well
+# but I found this way it's easier
 test_pings = ['ping 1',
               'ping 2',
               'ping 3',
@@ -23,25 +29,27 @@ test_pings = ['ping 1',
               'ping 8',
               'ping 9',
               'ping 10']
-
+# Using a For loop, going through the Array of messages for 10 times
 for message in test_pings:
-
     try:
-        clientSocket.sendto(message.encode(), (serverName, serverPort))
-        initial_time = time.time()
-        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-        ending_time = time.time()
-        now = datetime.now()
-        elapsed_time = str(ending_time - initial_time)
+        socket_client.sendto(message.encode(), (server_name, server_port))
+        init_time = time.time()                                      # Taking the initial time stamp
+        modifiedMessage, server_address = socket_client.recvfrom(2048)
+        end_time = time.time()                                       # Taking the End time stamp
+        current_date_time = datetime.now()                                            # Taking the current date and time
+        elapsed_time = str(end_time - init_time)                  # Subtracting the difference
 
-        print(modifiedMessage.decode(),
-              now.strftime("%a"),   # Formatting the time for month short version
-              now.strftime("%b"),   # Formatting for date
-              now.strftime("%d"),
-              now.strftime("%X"),
-              now.strftime("%G"))
+        # Print all the required outputs
+        # Formatters are used to printout data and time in required formats
+        # mentioned in the lab handout
+        print("Reply from", server_address[0], modifiedMessage.decode(),
+              current_date_time.strftime("%a"),
+              current_date_time.strftime("%b"),
+              current_date_time.strftime("%d"),
+              current_date_time.strftime("%X"),
+              current_date_time.strftime("%G"))
         print("RTT: ", elapsed_time)
     except timeout:
         print('Request Timed Out.')
 
-clientSocket.close()
+socket_client.close()
