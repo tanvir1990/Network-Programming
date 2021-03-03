6e63cdf9e3b8d779b398c7cfbfe43dd136c14441
tanvir1990 <pavel.mhp@gmail.com>
2021-03-02 19:58:58 -0500

    ATP works for multi thread on server

--- Assignments/server_multi_thread.py
+++ Assignments/server_multi_thread.py
@@ -6,19 +6,9 @@ import sys
 multicast_group = '224.3.29.71'
 server_address = ('', 10000)
 
-# Create the socket
+# UDP config
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# allow multiple server/receiver processes on the same computer to bind to the multicast port
-#
-# IMPORTANT
-#
-# Windows does not support SO_REUSEPORT, so we need to use REUSEADDR
-#
-# Linux/MacOS: need to use REUSEPORT
-#
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Bind to the server address
 sock.bind(server_address)
 
 # Tell the operating system to add the socket to the multicast group
@@ -27,14 +17,41 @@ group = socket.inet_aton(multicast_group)
 mreq = struct.pack('4sL', group, socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
+class ClientThread(threading.Thread):
+    def __init__(self, data, address, id):
+        threading.Thread.__init__(self)
+        self.data = data
+        self.address = address
+        self.id = id
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+
+    def run(self):
+        print("Start executing client thread: ", self.id)
+        capitalizedSentence = data.decode().upper()
+        sock.sendto(capitalizedSentence.encode(), address)
+        print("End executing client thread: ", self.id)
 
 # Receive/respond loop
+
+thread_count = 0
+threads = []
 while True:
     print('\nwaiting to receive message')
     data, address = sock.recvfrom(1024)
+    thread_count += 1
+    newthread = ClientThread(data, address, thread_count)
+    newthread.start()
+    threads.append(newthread)
+    time.sleep(5)
+    newthread.stop()
 
-    print('received %s bytes from %s' % (len(data), address))
-    print(data.decode())
+    if newthread.is_alive():
+        print("Thread still Alive")
+    else:
+        print("Killed")
 
-    print('sending acknowledgement to', address)
-    sock.sendto('ack'.encode(), address)
+for t in threads:
+     t.join()
